240a61c267ab6a3d598ff3d328a238f03faea14d
osmel1 <elhaoussama483@gmail.com>
2024-05-11 19:39:11 +0100

    add jwt .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterModule, RouterOutlet} from '@angular/router';
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink,NavBarComponent,RouterModule,  HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {CustomersComponent} from "./customers/customers.component";
+import {NewCustomerComponent} from "./new-customer/new-customer.component";
+import {CustomerAccountComponent} from "./customer-account/customer-account.component";
+import {AccountsComponent} from "./accounts/accounts.component";
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"login", component: LoginComponent},
+  {path:"",redirectTo:"login",pathMatch:"full"},
+  {path:'admin/customers', component: CustomersComponent},
+  {path:'new-customer', component: NewCustomerComponent},
+  {path:'accounts', component: AccountsComponent},
+  {path:'customer-account', component: CustomerAccountComponent },
+];
