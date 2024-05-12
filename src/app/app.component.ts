ee7f0d92c2d975b4cb63382e84ed90ab1e6b5bcf
osmel1 <elhaoussama483@gmail.com>
2024-05-12 20:35:14 +0100

    add guards authorization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink, RouterModule, RouterOutlet} from '@angular/router';
 import {NavBarComponent} from "./nav-bar/nav-bar.component";
 import {HttpClientModule} from "@angular/common/http";
+import {AuthService} from "./services/auth.service";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,14 @@ import {HttpClientModule} from "@angular/common/http";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
   title = 'front-end';
   protected readonly console = console;
+constructor(private authservice:AuthService) {
+}
+  ngOnInit(): void {
+    this.authservice.loadJwtTokenFromLocalStorage();
+  }
+
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,15 +8,18 @@ import {LoginComponent} from "./login/login.component";
 import {AdminTemplateComponent} from "./admin-template/admin-template.component";
 import {AuthenticationGuard} from "./guards/authentication.guard";
 import {EditCustomerComponent} from "./edit-customer/edit-customer.component";
+import {NotAutorizedComponent} from "./not-autorized/not-autorized.component";
+import {AuthorizationGuard} from "./guards/authorization.guard";
 
 export const routes: Routes = [
   {path:"login", component: LoginComponent},
   {path:"",redirectTo:"login",pathMatch:"full"},
   {path:"admin",component:AdminTemplateComponent, canActivate:[AuthenticationGuard],children:[
       {path:'customers', component: CustomersComponent},
-      {path:'new-customer', component: NewCustomerComponent},
+      {path:'new-customer', component: NewCustomerComponent , canActivate:[AuthorizationGuard],data : {roles:['ADMIN']} },
       {path:'edit-customer', component: EditCustomerComponent},
       {path:'accounts', component: AccountsComponent},
+      {path:'notAuthorized', component: NotAutorizedComponent},
       {path:'customer-account', component: CustomerAccountComponent },
       {path :"customer-accounts/:id", component : CustomerAccountComponent},
     ],},
